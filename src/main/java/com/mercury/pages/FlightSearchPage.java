e3db6cb189ee32cdf355eb6281e7f71b98f9b247
Swathi <Swathi@192.168.0.20>
2017-09-04 11:14:48 -0500

    Added Flight Search and Flight Test Pages

---
+++ src/main/java/com/mercury/pages/FlightSearchPage.java
@@ -0,0 +1,9 @@
+package com.mercury.pages;
+
+import org.testng.annotations.Test;
+
+public class FlightSearchPage {
+  @Test
+  public void f() {
+  }
+}
---
+++ src/main/java/com/mercury/tests/FlightSearchTest.java
@@ -0,0 +1,9 @@
+package com.mercury.tests;
+
+import org.testng.annotations.Test;
+
+public class FlightSearchTest {
+  @Test
+  public void f() {
+  }
+}
