c8523506f8f467260a315cb18577aab4087e35a9
Swathi <Swathi@192.168.0.20>
2017-09-05 13:01:13 -0500

    added alert fun

--- src/main/java/com/mercury/tests/FlightSearchTest.java
+++ src/main/java/com/mercury/tests/FlightSearchTest.java
@@ -1,9 +1,26 @@
 package com.mercury.tests;
 
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class FlightSearchTest {
+import com.mercury.pages.FlightSearchPage;
+import com.mercury.pages.MercurySignInPage;
+
+public class FlightSearchTest extends BaseTest
+{
+	
+	
   @Test
-  public void f() {
+  public void tc002_search() {
+		
+		RemoteWebDriver driver = getDriver();
+		  
+		  MercurySignInPage mTS = new MercurySignInPage(driver);		  
+		  mTS.loginToMercury("swathi", "swathi");
+		  
+		  FlightSearchPage fSP = new FlightSearchPage(driver);
+		  fSP.flightSearch();
+		  
+		  
   }
 }
