ec6d5090057c9e2acb2a833717860551e4cd9f18
Swathi <Swathi@192.168.0.20>
2017-09-05 13:09:10 -0500

    added prompt pages

---
+++ src/main/java/com/mercury/pages/PromptPage.java
@@ -0,0 +1,32 @@
+package com.mercury.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.mercury.tests.FunctionLibrary;
+
+public class PromptPage extends FunctionLibrary
+{
+	RemoteWebDriver driver; 
+	
+	@FindBy(how=How.XPATH, using="//button")
+	public WebElement _OnclickPromtButton;
+	
+	public Prompt(RemoteWebDriver driver)
+	{
+	this.driver = driver;
+	PageFactory.initElements(driver, this);
+	}
+	
+	public fnprompt()
+	{
+		driver.get("file:///C:/Selenium/prompt.html");
+		fnClick(_OnclickPromtButton);
+		
+	}
+  
+}
---
+++ src/test/java/com/mercury/tests/TestPrompt.java
@@ -0,0 +1,9 @@
+package com.mercury.tests;
+
+import org.testng.annotations.Test;
+
+public class TestPrompt {
+  @Test
+  public void f() {
+  }
+}
